244435a9109ece8bbadc9615c21a603eafd54b8d
ankita dangi <ankita.dangi@zingbytes.com>
2017-12-27 17:57:32 +0530

    add plans for tenants

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171227111148) do
+ActiveRecord::Schema.define(version: 20171227120910) do
+
+  create_table "members", force: :cascade do |t|
+    t.integer "tenant_id"
+    t.integer "user_id"
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tenant_id"], name: "index_members_on_tenant_id"
+    t.index ["user_id"], name: "index_members_on_user_id"
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.string "session_id", null: false
+    t.text "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
+    t.index ["updated_at"], name: "index_sessions_on_updated_at"
+  end
+
+  create_table "tenants", force: :cascade do |t|
+    t.integer "tenant_id"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "plan"
+    t.index ["name"], name: "index_tenants_on_name"
+    t.index ["tenant_id"], name: "index_tenants_on_tenant_id"
+  end
+
+  create_table "tenants_users", id: false, force: :cascade do |t|
+    t.integer "tenant_id", null: false
+    t.integer "user_id", null: false
+    t.index ["tenant_id", "user_id"], name: "index_tenants_users_on_tenant_id_and_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
